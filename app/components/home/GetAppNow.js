149a2657485acd054600c1113090283522fd4df6
vikram kumar <vikramkumar8t8@gmail.com>
2024-12-28 02:09:23 +0500

    update

--- app/components/home/GetAppNow.js
+++ app/components/home/GetAppNow.js
@@ -8,7 +8,7 @@ const GetAppNow = () => {
     <div className='w-full px-4 lg:px-6 xl:px-10 relative '>
       <div className='max-w-[1600px] mx-auto flex items-center justify-between xl:flex-row flex-col gap-6 sm:gap-10 w-full pt-12 md:pt-[100px] relative z-30'>
         <div>
-          <img src='/assets/mobiles.svg' alt='mobiles'/>
+          <img src='/assets/mobiles.svg' alt='mobiles' />
         </div>
         <div className='xl:max-w-[666px] pb-12 md:pb-[100px] xl:pb-0'>
           <h1 className='text-[24px] sm:text-[50px] md:text-[60px] md:leading-[70px] font-semibold font-workSpace tracking-[-0.02em] text-black'>Get <span className='text-[#EB3340]'>Sunlifter.Club</span> App now</h1>
@@ -21,7 +21,7 @@ const GetAppNow = () => {
           </Link>
         </div>
       </div>
-      <img src='/assets/right-image.svg' alt='right-image' className='w-[132px] absolute right-0 top-1/2 -translate-y-4/4 sm:translate-y-1/4' />
+      <img src='/assets/right-image.svg' alt='right-image' className='xl:w-[300px] w-[132px] absolute right-0 bottom-[40px] xl:top-[180px]' />
     </div>
   )
 }
